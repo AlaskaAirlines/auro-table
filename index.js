481b128b6c64bc4d739ac51f5872f76b944f9ef9
Jordan Jones <jmjones2463@gmail.com>
2024-09-20 10:39:26 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { AuroTable } from './src/auro-table.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
- const registerComponent = (name = 'custom-table') => {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroTable {});
-  }
-}
-
-export { registerComponent }
+RuntimeUtils.default.prototype.registerComponent('custom-table', AuroTable);
