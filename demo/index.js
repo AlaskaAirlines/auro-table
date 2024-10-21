73bc4fae6b8da9dea00c06eebefdaccffd17536d
Eunsun Mota <eunsun.mota@alaskaair.com>
2024-10-21 09:15:46 -0700

    feat(api): add register static method #92
    `AuroTable.register` is to easily register the element without extra importing
    `import "@aurodesignsystem/auro-table"` will still register this element to <auro-table>
    `import { AuroTable } from '../src/auro-table.js` wont register this element until `AuroTable.register` gets called
    
    Fixed the typo on Recommended Use and Version Control.
    `clement` -> `element`

---
+++ demo/index.js
@@ -0,0 +1,4 @@
+import { AuroTable } from '../src/auro-table.js';
+
+AuroTable.register(); // registering to `auro-table`
+AuroTable.register('custom-table');
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { AuroTable } from './src/auro-table.js';
-import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-RuntimeUtils.default.prototype.registerComponent('custom-table', AuroTable);
+AuroTable.register();
