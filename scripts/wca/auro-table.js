01f4e328c092c279775e4ad51ec29a0a769ecb1d
Joe Yang <joe.yang@alaskaair.com>
2025-04-02 22:48:41 -0700

    perf: add wca script for docs api

---
+++ scripts/wca/auro-table.js
@@ -0,0 +1,10 @@
+
+import { AuroTable } from '../../src/auro-table.js';
+
+/**
+     * */
+class AuroTableWCA extends AuroTable {}
+
+if (!customElements.get("auro-table")) {
+  customElements.define("auro-table", AuroTableWCA);
+}
